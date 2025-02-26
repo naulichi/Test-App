ba2bfb2d84ad950b0da168fa7026cc95de083582
naulichi <chiara@tschuba.ch>
2025-02-26 10:55:09 +0100

    first commit

---
+++ Start.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Meine erste Streamlit App")
+
+# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
+
+# Streamlit über den Text unten direkt in die App - cool!
+"""
+Diese App wurde von folgenden Personen entwickelt:
+- Chiara Nauli (naulichi@students.zhaw.ch)
+
+Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+Autor: Samuel Wehrli (wehs@zhaw.ch)
+"""
